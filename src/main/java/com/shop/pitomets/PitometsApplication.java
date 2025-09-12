4605574a2d00ca51415a997d7ad0792fa066dc17
Artem Sharkov <aartmoon@10.211.94.157-vpn.dhcp.yndx.net>
2025-09-12 22:19:55 +0300

    first structure

---
+++ src/main/java/com/shop/pitomets/PitometsApplication.java
@@ -0,0 +1,13 @@
+package com.shop.pitomets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PitometsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PitometsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shop/pitomets/PitometsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.pitomets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PitometsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
